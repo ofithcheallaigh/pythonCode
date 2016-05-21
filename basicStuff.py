717c053179d842cefd68bf6ebc03a586a99d239e
Seán <o.fithcheallaigh@gmail.com>
2016-05-21 11:01:58 +0100

    Update basicStuff.py

--- basicStuff.py
+++ basicStuff.py
@@ -1,6 +1,35 @@
 # This is taken from the Codecademy site.
 
+#Syntax
+
 # Booleans - data type. Just like a switch, either True or False
 # Variables can be used to store booleans:
 a = True
 b = False
+
+# Variable examples
+my_int = 7
+my_float = 1.23
+my_bool = True
+
+# Reassigning variables
+my_int = 9
+my_int = 3
+print(my_int)
+
+# Whitespace
+# In python, whitespace is sued to structure code. Whitespace is important, so you have to be careful
+# with how you use it. Below is badly formatted code, and produces an error
+def spam():
+eggs = 12
+return eggs
+  
+print spam()
+
+# Correct formatting is:
+def spam():
+	eggs = 12
+	return eggs
+
+print spam()
+
