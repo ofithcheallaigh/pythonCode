d4e0bca9f6d5c5d27921a58318d8e4292ee6e5b6
Seán <sofithcheallaigh@gmail.com>
2020-08-01 21:17:43 +0100

    Update object_detection.py
    Uncommenting code back into the script

--- Computer Vision/opencv_introduction/object_detection.py
+++ Computer Vision/opencv_introduction/object_detection.py
@@ -7,12 +7,12 @@ def nothing(x):
 cap = cv2.VideoCapture(0)
 
 cv2.namedWindow("Tracking")
-# cv2.createTrackbar("LH", "Tracking", 0, 255, nothing)       # LH = Lower Hue
-# cv2.createTrackbar("LS", "Tracking", 0, 255, nothing)       # LS = Lower Saturation
-# cv2.createTrackbar("LV", "Tracking", 0, 255, nothing)       # LV = Lower Value
-# cv2.createTrackbar("UH", "Tracking", 255, 255, nothing)       # UH = Upper Hue
-# cv2.createTrackbar("US", "Tracking", 255, 255, nothing)       # US = Upper Saturation
-# cv2.createTrackbar("UV", "Tracking", 255, 255, nothing)       # UV = Upper Value
+cv2.createTrackbar("LH", "Tracking", 0, 255, nothing)       # LH = Lower Hue
+cv2.createTrackbar("LS", "Tracking", 0, 255, nothing)       # LS = Lower Saturation
+cv2.createTrackbar("LV", "Tracking", 0, 255, nothing)       # LV = Lower Value
+cv2.createTrackbar("UH", "Tracking", 255, 255, nothing)       # UH = Upper Hue
+cv2.createTrackbar("US", "Tracking", 255, 255, nothing)       # US = Upper Saturation
+cv2.createTrackbar("UV", "Tracking", 255, 255, nothing)       # UV = Upper Value
 
 while True:
     frame = cv2.imread('smarties.png')
